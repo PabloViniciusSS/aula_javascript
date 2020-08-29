3aace3c1352f7a64022bd0759c04298bfd84034c
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2020-08-29 16:04:23 -0300

    js

---
+++ js/main.js
@@ -0,0 +1,50 @@
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado</b>"
+    console.log(document.getElementById("agradecimento"))
+}
+
+function redireciona()
+{
+    window.open("https://twitter.com/home")
+
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado pela presenca"
+   // alert("troca texto")
+}
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
+
+
+
+
+
+/*var count 
+for(count = 0; count <= 5; count++){
+    alert(count)
+}*/
+//var idade = prompt("Qual sua idade")
+//if (idade > 18){
+   // alert("maior de idade")
+//}else{
+   // alert("menor de idade")
+//}
+//var fruta = [{nome:"maçã",cor:"vermelho"}, {nome:"uva" , cor:"vinho"}]
+//console.log(fruta)
+//alert(fruta[1].nome)
+//var nome = "Pablo Vinícius"
+//var idade = 29
+//var idade2 = 25
+//var frase = "Japão melhor do mundo"
+//alert("Oi td bem "  + nome)
+//console.log(nome)
+//console.log(idade)
+//console.log(frase.toUpperCase())
+//var lista = ["mação","laranja"]
+//lista.push("uva")
+//console.log(lista[0])
+//console.log(lista.toString()[0])
+//console.log(lista.join(" "))
